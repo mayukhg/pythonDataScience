c033a76826a5bbce8db16186847f9e4953588db9
Mayukh <mayukhg@gmail.com>
2018-09-15 11:31:47 +0530

    Adding Code directory

---
+++ Code/test.py
@@ -0,0 +1,12 @@
+list1 = [0,2,3,4,5,6,7,8]
+list1[1:3]=[9,10,11]
+#print(list1)
+
+a= "Man"
+#print(type(a))
+
+set1 = {7, 2 , 13}
+#print(set1)
+
+mydict={1:2,2:4,3:6, 'test':2, 'check':'final'}
+print(mydict['check'])
